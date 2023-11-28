a3bde79ce3725c1e18c68a496350376a24d3e3d3
Haoson <739524658@qq.com>
2023-11-28 17:47:20 +0800

    rearrange project structure

--- include/parser_node.h
+++ include/parser_node.h
@@ -1,151 +1,5 @@
 #pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "symbol_table.h"
-typedef struct Type
-{
-    enum
-    {
-        PRIMITIVE,
-        ARRAY,
-        STRUCTURE,
-        FUNCTION
-    } category;
-    union
-    {
-        enum
-        {
-            SEMANTIC_TYPE_INT,
-            SEMANTIC_TYPE_FLOAT,
-            SEMANTIC_TYPE_CHAR
-        } primitive;
-        struct Array *array;
-        symbol_table *structure;
-        symbol_table *function;
-    };
-} Type;
-
-typedef struct Array
-{
-    struct Type *base;
-    int size;
-} Array;
-
-// int type_same_namespace(Type *type1, Type *type2)
-// {
-//     printf("type1->catagory: %d, type2->catagory: %d\n", type1->category, type2->category);
-//     return (type1->category == type2->category ||
-//             type1->category == PRIMITIVE && type2->category == ARRAY ||
-//             type1->category == ARRAY && type2->category == PRIMITIVE);
-// }
-
-int struct_equal(symbol_table *struct1, symbol_table *struct2)
-{
-    symbol_table_node *node1 = struct1->head;
-    symbol_table_node *node2 = struct2->head;
-    while (node1 != NULL && node2 != NULL)
-    {
-        if (!type_equal(node1->list->head->type, node2->list->head->type))
-        {
-            return 0;
-        }
-        node1 = node1->next;
-        node2 = node2->next;
-    }
-    return node1 == NULL && node2 == NULL;
-}
-
-int type_equal(Type *type1, Type *type2)
-{
-    if (type1 == NULL || type2 == NULL)
-    {
-        return 0;
-    }
-    if (type1->category != type2->category)
-    {
-        return 0;
-    }
-    switch (type1->category)
-    {
-    case PRIMITIVE:
-        if (type1->primitive != type2->primitive)
-        {
-            return 0;
-        }
-        break;
-    case ARRAY:
-        // if (type1->array->size != type2->array->size)
-        // {
-        //     return 0;
-        // }
-        if (!type_equal(type1->array->base, type2->array->base))
-        {
-            return 0;
-        }
-        break;
-    case STRUCTURE:
-        if (!struct_equal(type1->structure, type2->structure))
-        {
-            return 0;
-        }
-        break;
-    case FUNCTION:
-        if (!symbol_table_equal(type1->function, type2->function))
-        {
-            return 0;
-        }
-        break;
-    default:
-        break;
-    }
-    return 1;
-}
-
-void type_print(Type *type)
-{
-    if (type == NULL)
-    {
-        printf("NULL");
-        return;
-    }
-    switch (type->category)
-    {
-    case PRIMITIVE:
-        switch (type->primitive)
-        {
-        case SEMANTIC_TYPE_INT:
-            printf("int");
-            break;
-        case SEMANTIC_TYPE_FLOAT:
-            printf("float");
-            break;
-        case SEMANTIC_TYPE_CHAR:
-            printf("char");
-            break;
-        default:
-            break;
-        }
-        break;
-    case ARRAY:
-        // recursively print
-        printf("[%d]", type->array->size);
-        type_print(type->array->base);
-        break;
-    case STRUCTURE:
-        printf("struct {\n");
-        symbol_table_print(type->structure);
-        printf("}");
-        break;
-    case FUNCTION:
-        printf("function (\n");
-        symbol_table_print(type->function);
-        printf(")");
-        break;
-    default:
-        break;
-    }
-}
 typedef struct ParserNode
 {
     char name[20];
@@ -165,183 +19,49 @@ typedef struct ParserNode
 
 } ParserNode;
 
-void passType(struct ParserNode *node, Type *type)
+typedef struct Type
 {
-    if (strcmp(node->name, "ExtDecList") == 0 ||
-        strcmp(node->name, "DecList") == 0)
+    enum
     {
-        for (int i = 0; i < node->child_num; i++)
+        PRIMITIVE,
+        ARRAY,
+        STRUCTURE,
+        FUNCTION
+    } category;
+    union
+    {
+        enum
         {
-            passType(node->child[i], type);
-        }
-    }
-    if (strcmp(node->name, "VarDec") == 0 ||
-        strcmp(node->name, "Dec") == 0)
-    {
-        memcpy(node->type, type, sizeof(Type));
-    }
-}
+            SEMANTIC_TYPE_INT,
+            SEMANTIC_TYPE_FLOAT,
+            SEMANTIC_TYPE_CHAR
+        } primitive;
+        struct Array *array;
+        SymbolTable *structure;
+        SymbolTable *function;
+    };
+} Type;
 
-void addParserNode(struct ParserNode *node, struct ParserNode *child)
+typedef struct Array
 {
-    node->child[node->child_num++] = child;
-    if (node->type == NULL)
-        node->type = child->type;
-}
+    struct Type *base;
+    int size;
+} Array;
 
-void setParserNodeType(struct ParserNode *node, char *type_name)
-{
-    node->type = (struct Type *)malloc(sizeof(struct Type));
-    if (strcmp(type_name, "int") == 0)
-    {
-        node->type->category = PRIMITIVE;
-        node->type->primitive = SEMANTIC_TYPE_INT;
-    }
-    else if (strcmp(type_name, "float") == 0)
-    {
-        node->type->category = PRIMITIVE;
-        node->type->primitive = SEMANTIC_TYPE_FLOAT;
-    }
-    else if (strcmp(type_name, "char") == 0)
-    {
-        node->type->category = PRIMITIVE;
-        node->type->primitive = SEMANTIC_TYPE_CHAR;
-    }
-}
+// int type_same_namespace(Type *type1, Type *type2);
 
-ParserNode *initParserNode(const char *name, int lineno)
-{
-    ParserNode *node = (struct ParserNode *)malloc(sizeof(struct ParserNode));
-    strcpy(node->name, name);
-    node->line = lineno;
-    node->to_print_lineno = 0;
-    node->child_num = 0;
-    node->empty_value = 0;
-    node->is_left_value = 0;
-    memset(node->child, 0, sizeof(node->child));
-    node->type = NULL;
-    // printf("init %s\n", node->name);
-    return node;
-}
+int struct_equal(SymbolTable *struct1, SymbolTable *struct2);
+int type_equal(Type *type1, Type *type2);
+void type_print(Type *type);
+void passType(struct ParserNode *node, Type *type);
+void addParserNode(struct ParserNode *node, struct ParserNode *child);
+void setParserNodeType(struct ParserNode *node, char *type_name);
 
-void cal_line(struct ParserNode *node)
-{
-    node->to_print_lineno = 1;
-    // for (int i = 0; i < node->child_num; i++) {
-    //     node->line = node->line > node->child[i]->line ? node->child[i]->line : node->line;
-    // }
-    node->line = node->child[0]->line;
-}
+ParserNode *initParserNode(const char *name, int lineno);
+void cal_line(struct ParserNode *node);
+void printParserNode(struct ParserNode *node, int depth);
 
-void printParserNode(struct ParserNode *node, int depth)
-{
-    // print tabs according to depth
-    if (node == NULL)
-        return;
-    if (node->empty_value)
-        return;
-    for (int i = 0; i < depth; i++)
-    {
-        printf("  ");
-    }
-    printf("%s", node->name);
-    if (node->to_print_lineno == 1)
-    {
-        printf(" (%d)", node->line);
-    }
-    if (strcmp(node->name, "INT") == 0)
-    {
-        printf(": %d", node->value.int_value);
-    }
-    else if (strcmp(node->name, "FLOAT") == 0)
-    {
-        // printf(": %f", node->value.float_value);
-        printf(": %s", node->value.string_value);
-    }
-    else if (strcmp(node->name, "CHAR") == 0)
-    {
-        printf(": %s", node->value.string_value); // some day after the phase test, I will delete this
-    }
-    else if (strcmp(node->name, "ID") == 0)
-    {
-        printf(": %s", node->value.string_value);
-    }
-    else if (strcmp(node->name, "TYPE") == 0)
-    {
-        printf(": %s", node->value.string_value);
-    }
-    else if (strcmp(node->name, "LITERAL") == 0)
-    {
-        printf(": %s", node->value.string_value);
-    }
-    printf("\n");
-    for (int i = 0; i < node->child_num; i++)
-    {
-        printParserNode(node->child[i], depth + 1);
-    }
-}
-
-int var_declare(symbol_table *global_table, scope_list *stack, char *name, Type *type)
-{
-    symbol_table *table = stack->head->table;
-    symbol_table_node *node = symbol_table_find(table, name);
-    if (node != NULL)
-    {
-        return 1;
-    }
-    symbol_table_add_node(table, symbol_table_insert(global_table, name, type));
-    return 0;
-}
-
-int function_declare(symbol_table *global_table, char *name, Type *type)
-{
-    symbol_table_node *node = symbol_table_find(global_table, name);
-    if (node != NULL)
-    {
-        return 1;
-    }
-    symbol_table_insert(global_table, name, type);
-    return 0;
-}
-
-int check_return_type(ParserNode *ParserNode, Type *type)
-{
-    if (ParserNode == NULL)
-        return 1;
-
-    if (strcmp(ParserNode->name, "Stmt") == 0 && strcmp(ParserNode->child[0]->name, "RETURN") == 0)
-    {
-        if (!type_equal(ParserNode->child[1]->type, type))
-        {
-            printf("Error type 8 at Line %d: Type mismatched for return.\n", ParserNode->child[0]->line);
-        }
-        return 0;
-    }
-    int ret = 1;
-    for (int i = 0; i < ParserNode->child_num; i++)
-    {
-        ret = check_return_type(ParserNode->child[i], type) && ret;
-    }
-    return ret;
-}
-
-int check_function_args(symbol_table *function, symbol_table *args)
-{
-    symbol_table_node *node1 = function->head->next;
-    if (args == NULL)
-    {
-        return node1 != NULL;
-    }
-    symbol_list_node *node2 = args->head->list->head;
-    while (node1 != NULL && node2 != NULL)
-    {
-        if (!type_equal(node1->list->head->type, node2->type))
-        {
-            return 1;
-        }
-        node1 = node1->next;
-        node2 = node2->next;
-    }
-    return node1 != NULL || node2 != NULL;
-}
-
+int var_declare(SymbolTable *global_table, ScopeList *stack, char *name, Type *type);
+int function_declare(SymbolTable *global_table, char *name, Type *type);
+int check_return_type(ParserNode *ParserNode, Type *type);
+int check_function_args(SymbolTable *function, SymbolTable *args);
--- include/string_map.h
+++ include/string_map.h
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#pragma once
 #define TABLE_SIZE 100
 
 typedef struct {
@@ -13,68 +10,10 @@ typedef struct {
     Entry* entries[TABLE_SIZE];
 } Map;
 
-unsigned long hash(const char* str) {
-    unsigned long hash = 5381;
-    int c;
-    while ((c = *str++)) {
-        hash = ((hash << 5) + hash) + c;
-    }
-    return hash % TABLE_SIZE;
-}
-
-void initMap(Map* map) {
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        map->entries[i] = NULL;
-    }
-}
-
-void setMap(Map* map, const char* key, const char* value) {
-    unsigned long index = hash(key);
-    Entry* entry = map->entries[index];
-    if (entry == NULL) {
-        entry = (Entry*)malloc(sizeof(Entry));
-        entry->key = strdup(key);
-        map->entries[index] = entry;
-    }
-    entry->value = strdup(value);
-}
-
-char* getMap(Map* map, const char* key) {
-    unsigned long index = hash(key);
-    Entry* entry = map->entries[index];
-    if (entry == NULL) {
-        return NULL;
-    }
-    return entry->value;
-}
-
-void deleteMap(Map* map, const char* key) {
-    unsigned long index = hash(key);
-    Entry* entry = map->entries[index];
-    if (entry != NULL) {
-        free(entry->key);
-        free(entry->value);
-        free(entry);
-        map->entries[index] = NULL;
-    }
-}
-
-void freeMap(Map* map) {
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        Entry* entry = map->entries[i];
-        if (entry != NULL) {
-            free(entry->key);
-            free(entry->value);
-            free(entry);
-        }
-    }
-}
-
-void printMap(Map* map) {
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        Entry* entry = map->entries[i];
-        if (entry != NULL) {
-            printf("%s: %s\n", entry->key, entry->value);
-        }
-    }
-}
+unsigned long hash(const char* str);
+void initMap(Map* map);
+void setMap(Map* map, const char* key, const char* value);
+char* getMap(Map* map, const char* key);
+void deleteMap(Map* map, const char* key);
+void freeMap(Map* map);
+void printMap(Map* map);
